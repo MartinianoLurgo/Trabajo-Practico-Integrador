d2d0874d393c62deadc55d74549197f5643b6056
nicolas porello <nicolasporello9@gmial.com>
2023-06-26 01:01:37 -0300

    cambio vistaItem

---
+++ MVC/view/vistaItem.py
@@ -0,0 +1,17 @@
+class VistaItem:
+    def importe(self,servicio):
+        return(f"Importe de servicio :{servicio}⛽")
+        
+    def mostrar_iva(self,iva):
+        return (f" IVA total: {iva}😮")
+    
+    def mostrar_senia(self,senia):
+        return(f"La seña que dio el cliente es de: {senia}💵")
+    
+    def mostrar_total(self,total):
+        return (f"El total de servico: {total} 🛒")
+    
+    def mostrar_devolucion(self,devolucion):
+        return (f"Devolucion: {devolucion}👌")
+        
+    
