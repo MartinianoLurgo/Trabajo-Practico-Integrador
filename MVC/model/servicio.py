168ae6bc1b1dec81074797d6a0ca8d5b22041090
Federico Piñan <85468675+federicopinan@users.noreply.github.com>
2023-06-26 00:30:37 -0300

    Actualización de modelos
    Atributos y métodos de modelos

--- MVC/model/servicio.py
+++ MVC/model/servicio.py
@@ -1,23 +1,38 @@
 class Servicio:
-    def __init__(self) -> None:
-        self._id 
+    def __init__(self):
+        self._idServicio
+        self._tipoServicio
+        self._descripcion
         self._costo
-        self._listaServicios
+        self._estado
+        self._listaServicios = []
     #Getters
     def get_id(self):
-        return self._id
+        return self._idServicio
+    def get_tipoServicio(self):
+        return self._tipoServicio
+    def get_descripcion(self):
+        return self._descripcion
     def get_costo(self):
         return self._costo
+    def get_estado(self):
+        return self._estado
     def get_listaServicios(self):
         return self._listaServicios
     
     #Setters
     def set_id(self,data):
-        self._id = data
+        self._idServicio = data
+    def set_tipoServicio(self,data):
+        self._tipoServicio = data
+    def set_descripcion(self,data):
+        self._descripcion = data
     def set_costo(self,data):
         self._costo = data
+    def set_estado(self,data):
+        self._estado = data
     def set_litaServicios(self,data):
         self._listaServicios = data
     
     def __str__(self) -> str:
-        return print(f"Id: {self._id}, Costo: {self._costo}, Lista de servicios: {self._listaServicios}")
+        return print(f"Id: {self._idServicio}, Tipo de servicio: {self._tipoServicio}, Descripcion: {self._descripcion}, Costo: {self._costo}, Estado: {self._estado}, Lista de servicios: {self._listaServicios}")
