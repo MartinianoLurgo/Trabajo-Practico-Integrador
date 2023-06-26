8abdf8a9a78de9278c4175c92d96df96523a1f70
Charras <elu_embo@hotmail.com>
2023-06-26 00:38:46 -0300

    Cambio VISTA cliente.

---
+++ MVC/view/vistaCliente.py
@@ -0,0 +1,41 @@
+
+class VistaCliente:
+    def mostrar_menu(self):
+        print("🔅================🔸🔱🔸================🔅")
+        print("       BIENVENIDO AL MENU DE CLIENTES")
+        print("🔅================🔸⚜️🔸================🔅") 
+        print("")
+        print("1 - Mostrar Lista de Clientes ")
+        print("2 - Cambiar Datos del Cliente")
+        print("2 - Salir del Programa")
+    
+    
+    def pedir_opcion(self):
+        return int(input("➡️ "))
+    
+    def archivo_no_encontrado(self):
+        print("No se encontró ningún archivo de clientes . Se creará uno nuevo.")
+    
+    def pedir_nombre(self):
+        return input("Ingrese el Nombre: ")
+    
+    def pedir_apellido(self):
+        return input("Ingrese el Apellido: ")
+    
+    def pedir_dni(self):
+        return int(input("Ingrese el número de DNI del cliente: "))
+    
+    def pedir_telefono(self):
+        return int(input("Ingrese el teléfono 📞: "))
+    
+    def pedir_metodo_de_pago(self):
+        return input("Ingrese el Metodo de Pago del Cliente💲: ")
+    
+    def dato_invalido(self):
+        print("Error, dato ingresado no válido ❌.")
+    
+    def registro_exitoso(self):
+        print("Se Registro el cliente ✅.")
+    
+    def cliente_no_encontrado(self):
+        print("No se encontró el cliente ❌.")
