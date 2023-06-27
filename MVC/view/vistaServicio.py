8716d158a8160fca3279b2518b146826f6e88bf5
NachoMansilla2103 <117238991+NachoMansilla2103@users.noreply.github.com>
2023-06-27 00:02:37 -0300

    Update vistaServicio.py

--- MVC/view/vistaServicio.py
+++ MVC/view/vistaServicio.py
@@ -1,10 +1,10 @@
 class VistaServicio:
-    def mostrar_listaServicios(self):
-        print("==================")
-        print("LISTA DE SERVICIOS")
-        print("==================")
+    def mostrar_listaServicios(self,servicio):
+        print(servicio)
+        
+    def elegir_servicio(self):
+        print("Ingrese el Numero de identificacion del Servicio🎉: ")
+
+    def modificar_precio(self):
+        print("Ingresar el Nuevo Precio💵: ")
         print("")
-        print("- DJ\n- Decoración\n - Cotillon\n- Maquina de humo\n- Maquillaje\n- Musica en vivo")
-    
-    def costo_dj(self):
-        return float(input("Costo -> "))
