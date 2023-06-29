ce32bc519a722ef41b93a9f72283233b2c476d26
Federico Piñan <85468675+federicopinan@users.noreply.github.com>
2023-06-28 21:47:08 -0300

    Mas cambios

--- MVC/controller/controladorCliente.py
+++ MVC/controller/controladorCliente.py
@@ -1,8 +1,10 @@
 from model.cliente import Cliente
 from view.vistaCliente import VistaCliente
+from view.vistaReserva import VistaReserva
 
 class ControladorCliente:
     def __init__(self):
+<<<<<<< Updated upstream
         self._modelo = Cliente()
         self._vista = VistaCliente()
         self._clientes = []
@@ -65,3 +67,43 @@ class ControladorCliente:
                 
                 
 
+=======
+        self._modelo = Cliente(idCliente=0,nombre="",apellido="",dni=0,telefono=0,metodoDePago="")
+        self._vistaCliente = VistaCliente()
+        self._vistaReserva = VistaReserva()
+        # self._archivo = archivo
+        self._listaClientes = []
+    
+    def cargar_archivo_clientes(self):
+        try:
+            with open(self.archivo, "r", encoding="utf-8") as archivo:
+                for lineas in archivo.readlines():
+                    linea = lineas.strip().split(",")
+                    self._modelo = Cliente(linea[0], linea[1], linea[2], linea[3], linea[4], linea[5])
+                    self._listaClientes.append(self._modelo)
+        except FileNotFoundError:
+            self._vista.archivo_noEncontrado()
+    
+    def menu_clientes(self):
+        opcion = 0
+        while opcion != 3:
+            self._vistaReserva.limpiar_pantalla()
+            self._vistaCliente.mostrar_menu_clientes()
+            try:
+                opcion = self._vistaCliente.pedir_opcion()
+                if opcion < 1 or opcion > 3:
+                    self._vistaReserva.limpiar_pantalla()
+                    self._vistaCliente.dato_invalido()
+                    self._vistaReserva.mostrar_mensaje_continuar()
+            except ValueError:
+                self._vistaReserva.limpiar_pantalla()
+                self._vistaCliente.dato_invalido()
+                self._vistaReserva.mostrar_mensaje_continuar()
+            match opcion:
+                case 1:
+                    with open(self._archivo,"r") as archivo:
+                        lineas = archivo.readlines()
+                        for linea in lineas:
+                            linea = lineas.strip().split(",")
+                            print(linea)
+>>>>>>> Stashed changes
--- MVC/main.py
+++ MVC/main.py
@@ -1,2 +1,7 @@
 if __name__=="__main__":
+<<<<<<< Updated upstream
     print("")
+=======
+    controlador = ControladorReserva("Archivos\\clientes.txt","Archivos\\fechas.txt","Archivos\\servicios.txt","Archivos\\gastosAdministrativos.txt", )
+    controlador.mostrar_menu()
+>>>>>>> Stashed changes
