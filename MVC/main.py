3e8acb7171f5ef3a5dc3d4ac562222116e288d12
nicolas porello <nicolasporello9@gmial.com>
2023-06-30 00:53:18 -0300

    Update main.py

--- MVC/main.py
+++ MVC/main.py
@@ -1,8 +1,9 @@
 """from controller.controladorReserva import ControladorReserva"""
 from controller.controladorCliente import controladorcliente
 if __name__=="__main__":
-   """ print("")
+   """print("")
     controlador = ControladorReserva("Archivos\\clientes.txt","Archivos\\fechas.txt","Archivos\\servicios.txt","Archivos\\gastosAdministrativos.txt", )
-    controlador.mostrar_menu()"""
+    controlador.mostrar_menu()"""# fede tuve que comentar esto porque sino no me anda ya q no entiendo lo q vas a ghacvert aca +
+   
    control = controladorcliente()
    control.menu()
